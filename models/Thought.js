e9bdbca2952edeef95b9ae09e6a315c61907f781
zacktmiller92 <zacktmiller92@gmail.com>
2021-10-17 20:06:25 -0400

    fixed typo

--- models/Thought.js
+++ models/Thought.js
@@ -52,7 +52,7 @@ const ThoughtSchema = new Schema(
     }
 );
 
-thoughtSchema.virtual('reactionCount').get(function () {
+ThoughtSchema.virtual('reactionCount').get(function () {
     return this.reactions.length;
 });
 
--- models/User.js
+++ models/User.js
@@ -39,7 +39,7 @@ const UserSchema = new Schema(
 );
 
 
-userSchema.virtual('friendCount').get(function () {
+UserSchema.virtual('friendCount').get(function () {
     return this.friends.length;
 });
 
