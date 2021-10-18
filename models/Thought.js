ae65fae618e1d1f987577efe20dc22df27f82e4a
zacktmiller92 <zacktmiller92@gmail.com>
2021-10-17 20:03:32 -0400

    added virtual for reaction count on Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -52,7 +52,9 @@ const ThoughtSchema = new Schema(
     }
 );
 
-
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
 
 const Thought = model('Thought', ThoughtSchema);
 
